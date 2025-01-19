9335232c937b2374c480dfd4758582a6434365ab
kingstar <admin@baks.dev>
2025-01-19 18:03:38 +0300

    Baks Development

--- Controller/Admin/NewController.php
+++ Controller/Admin/NewController.php
@@ -1,6 +1,6 @@
 <?php
 /*
- *  Copyright 2024.  Baks.dev <admin@baks.dev>
+ *  Copyright 2025.  Baks.dev <admin@baks.dev>
  *  
  *  Permission is hereby granted, free of charge, to any person obtaining a copy
  *  of this software and associated documentation files (the "Software"), to deal
@@ -54,9 +54,9 @@ final class NewController extends AbstractController
 
         $ProductDTO = new MaterialDTO();
 
-        if($this->getAdminFilterProfile())
+        if($this->isAdmin() === false)
         {
-            $ProductDTO->getInfo()->setProfile($this->getAdminFilterProfile());
+            $ProductDTO->getInfo()->setProfile($this->getProfileUid());
         }
 
         // Если передан идентификатор события - копируем
